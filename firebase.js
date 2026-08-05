52344b989d26d3f971ac9766f6ec1ff4c61ed592
jax254 <jax16979@gmail.com>
2026-08-05 22:33:07 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,60 +1,9 @@
-// Firebase SDK Imports
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+const supabaseUrl = "https://mreqwrdkucggwvxvturl.supabase.co";
 
-import { 
-    getAuth 
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+const supabaseKey = "YOUR_SB_PUBLISHABLE_KEY";
 
-import { 
-    getFirestore 
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-import { 
-    getStorage 
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
-
-
-// Your Firebase Configuration
-
-const firebaseConfig = {
-
-  apiKey:
-
-  authDomain: "prudence-kas.firebaseapp.com",
-
-  projectId: "prudence-kas",
-
-  storageBucket: "prudence-kas.firebasestorage.app",
-
-  messagingSenderId: "98599014078",
-
-  appId: "1:98599014078:web:6118132c134024906f58ef",
-
-  measurementId: "G-LBFLVVZ6X7"
-
-};
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-
-
-// Firebase Services
-
-const auth = getAuth(app);
-
-const db = getFirestore(app);
-
-const storage = getStorage(app);
-
-
-// Export for other files
-
-export {
-    app,
-    auth,
-    db,
-    storage
-};
+export default supabase;
