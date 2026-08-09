3681c70bc117de19ce7d77fe6662163e32ce7829
jax254 <jax16979@gmail.com>
2026-08-09 21:41:45 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabaseUrl = "https://mreqwrdkucggwvxvturl.supabase.co";
-
-const supabaseKey = "sb_publishable_5wGX_HMaRoWb63uDX6hRyw_gxwm4JCR";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
